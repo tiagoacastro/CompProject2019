a13da202c28bc34d60342ea7e96dab491b47fe62
Duarte Oliveira <duartefmoliveira8@gmail.com>
2019-03-21 17:25:52 +0000

    Removed my constants file

--- MyConstants.java
+++
@@ -1,9 +0,0 @@
-public class MyConstants {
-    public static final int COMMERCIALE = 1;
-    public static final int MINOR = 2;
-    public static final int ADD = 3;
-    public static final int MINUS = 4;
-    public static final int ASTERISC = 5;
-    public static final int SLASH = 6;
-    public static final String[] ops = {"&&","<","+","-","*","/"};
-}
