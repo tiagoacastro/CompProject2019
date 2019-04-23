d8c387d455f47aaeb352d72a333d72e377d60b0c
Tiago Castro <tiagoaraujocastro@gmail.com>
2019-04-23 17:18:31 +0100

    more stuff

--- SymbolTable.java
+++ SymbolTable.java
@@ -2,5 +2,13 @@ import java.util.HashMap;
 
 public class SymbolTable {
     private SymbolTable parent;
-    private HashMap<String, Symbol> symbols;
+    private HashMap<String, String> symbols; //Identifier -> Type
+
+    public void setParent(SymbolTable parent){
+        this.parent = parent;
+    }
+
+    public void addSymbol(String type, String identifier){
+        symbols.put(identifier, type);
+    }
 }
