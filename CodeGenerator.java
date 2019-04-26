8acdd308d70aa3317517f19ede4adfb55d639457
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2019-04-26 17:47:15 +0100

    added code generator

---
+++ CodeGenerator.java
@@ -0,0 +1,61 @@
+import java.io.PrintWriter;
+
+public class CodeGenerator {
+
+    private SimpleNode root;
+    private PrintWriter output;
+    private StringBuilder builder;
+
+    public CodeGenerator(SimpleNode root) {
+        this.root = (SimpleNode) root.children[0];
+
+        this.builder = new StringBuilder();
+
+        String filename = this.root.value + ".j";
+
+        FileWriter fileWriter = new FileWriter(filename, false);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        this.output = new PrintWriter(bufferedWriter);
+
+    }
+
+    public void generate() {
+        generateHeader();
+        generateGlobals();
+        generateMethods();
+
+        // outputs the final string builded to the file
+        output.println(builder);
+    }
+
+    private void generateHeader() {
+        builder.append(".class public ");
+        builder.append(root.jjtGetValue());
+        builder.append("\n");
+        builder.append(".super java/lan/Object");
+        builder.append("\n");
+    }
+
+    private void generateGlobals() {
+
+        for (int i = 0; i < root.jjtGetNumChildren(); i++) {
+            SimpleNode child = (SimpleNode) root.jjtGetChild(i);
+
+            // if()
+        }
+    }
+
+    private void generateMethods() {
+
+        for (int i = 0; i < root.jjtGetNumChildren(); i++) {
+            SimpleNode childRoot = (SimpleNode) root.jjtGetChild(i);
+
+            // if()
+        }
+    }
+
+    private void generateOperations() {
+        
+    }
+}
