8ea5659cf897f8b16660465dfc5a413e4b2559ce
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2019-04-28 17:05:00 +0100

    && and < checks done

--- ASTCOMMERCIALE.java
+++ ASTCOMMERCIALE.java
@@ -11,11 +11,38 @@ class ASTCOMMERCIALE extends SimpleNode {
     }
 
     public void applySemanticAnalysis(SymbolTable table) {
-        if (children == null) return;
+        SimpleNode lhs = ((SimpleNode) children[0]);
+        SimpleNode rhs = ((SimpleNode) children[1]);
 
-        for (int i = 0; i < children.length; i++) {
-            ((SimpleNode) children[i]).applySemanticAnalysis(table);
+        checkSide(lhs, table);
+        checkSide(rhs, table);
+    }
+
+    public void checkSide(SimpleNode side, SymbolTable table) {
+        if (side.name != null) {
+            Symbol s = table.getSymbol(side.name);
+            if (s != null) {
+                if (!s.isInitialized()) {
+                    System.out.println("Variable " + side.name + " not initialized on line " + side.getLine());
+                    System.exit(0);
+                }
+                if (!s.getType().equals("boolean")) {
+                    System.out.println("Incompatible type: " + side.name + " not of type boolean on line " + side.getLine());
+                    System.exit(0);
+                }
+            }
+            return;
+        } 
+
+        if (side instanceof ASTTRUE && side instanceof ASTFALSE) return; 
+
+        if (side instanceof ASTMINOR || side instanceof ASTCOMMERCIALE) {
+            side.applySemanticAnalysis(table);
+            return;
         }
+
+        System.out.println("Found " + side.toString() + " and was expecting <, && or boolean value on line " + side.getLine());
+        System.exit(0);
     }
 }
 /* JavaCC - OriginalChecksum=e89498b22068852d8629e30d2f1a5a9d (do not edit this line) */
