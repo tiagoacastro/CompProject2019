314161ba8151c2089c7edccb02d0d1df759f6b3c
Tiago Castro <tiagoaraujocastro@gmail.com>
2019-06-06 18:00:13 +0100

    started some sutff, might not follow up on it

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# sh run.sh test_class_name
+# sh run.sh test_class_name [-r N]
 
 jjtree parser.jjt
 javacc parser.jj
 javac *.java
-java JmmParser $1.jmm
+java JmmParser $1.jmm $2 $3
 java -jar jasmin.jar $1.j
